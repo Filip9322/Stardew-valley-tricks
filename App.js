ebeeee0dcf81d38136d5b3fa3d7a168e838aca1c
filip9322 <filip9322@gmail.com>
2019-10-06 13:50:53 -0500

    Setting a text field and butto, you should read a lot of documentation

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-  const [ outputText, setOutputText ] = useState('Open up App.js to start working on your app!');
   return (
-    <View style={styles.container}>
-      <Text>{ outputText }</Text>
-      <Button title ="Change text"  onPress={() => setOutputText('The text changed!')}/>
+    <View style={{padding: 30}}>
+      <Text>Stardew Valley Tricks and Calculator</Text>
+      <View>
+        <TextInput placeholder="Set Goal..." style={{borderBottomWidth: 1, borderBottomColor: 'black', padding: 10}}/>
+        <Button title="ADD"/>
+      </View>
+      <View>
+      
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
