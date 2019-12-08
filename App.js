480c96762bc002e97215c2e24d59e917a22dc406
Soulfilip <filip9322@gmail.com>
2019-12-08 17:27:21 -0500

    Example of eventsHandler with React Hooks

--- App.js
+++ App.js
@@ -2,12 +2,28 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [ enteredGoal, setEnteredGoal ] = useState('');
+  const [ courseGoals, setCourseGoals ] = useState([]);
+
+  const goalInputHandler = (enteredText) =>{
+    setEnteredGoal(enteredText);
+  }
+  const addGoalHandler   = () => {
+    //setCourseGoals([...courseGoals, enteredGoal]);
+    setCourseGoals( currentGoals => [...currentGoals, enteredGoal]);
+  }
+
   return (
     <View style={ styles.screen }>
       <Text>Stardew Valley Tricks and Calculator</Text>
       <View style={ styles.contRow }>
-        <TextInput style={ styles.txtInput } placeholder="Set Goal..."/>
-        <Button style={ styles.bt } title="ADD"/>
+        <TextInput
+          placeholder="Set Goal..."
+          style={ styles.txtInput }
+          onChangeText= { goalInputHandler }
+          value = { enteredGoal }
+        />
+        <Button style={ styles.bt } title="ADD" onPress={ addGoalHandler } />
       </View>
     </View>
   );
@@ -15,9 +31,9 @@ export default function App() {
 
 const styles  = StyleSheet.create({
   contRow : { flexDirection: 'row' , alignItems: 'flex-end'},
-  screen: { padding: 30 },
+  screen  : { padding: 30 },
   txtInput: { borderBottomWidth: 1, borderBottomColor: 'black',
               padding: 10, width: '80%'
             },
-  bt : { padding:'10 0' }
+  bt      : { padding:'10 0' }
 });
