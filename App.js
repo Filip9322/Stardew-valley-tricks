bef6a91e1d8d5016a2a763e2d13ec93ee480d7ef
Soulfilip <filip9322@gmail.com>
2019-12-08 15:10:54 -0500

    Example with StyleSheet component

--- App.js
+++ App.js
@@ -3,15 +3,21 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding: 30}}>
+    <View style={ styles.screen }>
       <Text>Stardew Valley Tricks and Calculator</Text>
-      <View>
-        <TextInput placeholder="Set Goal..." style={{borderBottomWidth: 1, borderBottomColor: 'black', padding: 10}}/>
-        <Button title="ADD"/>
-      </View>
-      <View>
-      
+      <View style={ styles.contRow }>
+        <TextInput style={ styles.txtInput } placeholder="Set Goal..."/>
+        <Button style={ styles.bt } title="ADD"/>
       </View>
     </View>
   );
 }
+
+const styles  = StyleSheet.create({
+  contRow : { flexDirection: 'row' , alignItems: 'flex-end'},
+  screen: { padding: 30 },
+  txtInput: { borderBottomWidth: 1, borderBottomColor: 'black',
+              padding: 10, width: '80%'
+            },
+  bt : { padding:'10 0' }
+});
---
+++ README.md
@@ -0,0 +1,12 @@
+# Stardew-valley-tricks :fleur_de_lis:
+This project is based on React Native and it's proposal is completely educative .
+
+## 🛠️ In order to launh the project
+```shell
+     /* Install dependencies*/
+     npm i
+
+     /* Launch expo cli*/
+     npm start
+```
+You could use any mobile simulator and with in the expo app and the link (QR). Th app would run pn the phone.
